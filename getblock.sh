e2c0b05a3f3f70f1f4d18c7a4a866ff97e66db44
user <user@flibustier.devices.gausi.fr>
2020-04-10 18:32:08 +0200

    first commit

---
+++ getblock.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+curl -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0"\
+-s https://www.iblocklist.com/lists.php \
+  | sed -n "s/.*value='\(http:.*\)'.*/\1/p" \
+  | xargs wget -O - \
+  | gunzip \
+  | egrep -v '^#'
